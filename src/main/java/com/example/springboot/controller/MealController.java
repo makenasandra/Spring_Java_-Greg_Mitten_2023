f06c8ec529cff031eb54ef4ee1180e130bb88604
Greg <gregoryamitten@gmail.com>
2023-02-12 13:05:04 +0100

    12

--- src/main/java/com/example/springboot/controller/MealController.java
+++ src/main/java/com/example/springboot/controller/MealController.java
@@ -5,12 +5,14 @@ import com.example.springboot.model.Meal;
 import com.example.springboot.service.MealService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/meal")
 public class MealController {
     private MealService mealService;
 
@@ -19,7 +21,7 @@ public class MealController {
         this.mealService = mealService;
     }
 
-    @GetMapping("/meals")
+    @GetMapping("/list")
     public ResponseEntity<List<Meal>> getMeals() {
         return ResponseEntity.ok(mealService.getMeals());
     }
@@ -45,6 +47,11 @@ public class MealController {
     @GetMapping("/summer-meals")
     public ResponseEntity<List<Meal>> getSummerMeals() {
         return ResponseEntity.ok(mealService.getSummerMeals());
+    }
+
+    @GetMapping("/secret-formula")
+    public ResponseEntity<String> getSecretFormula() {
+        return ResponseEntity.ok("2 + 2 = 4");
     }
 
 }
