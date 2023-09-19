8c21a05aa7e5df5e5cd6283711e848af2f56e4a2
makenasandra <makenawachira@gmail.com>
2023-09-19 10:04:50 +0300

    exercise 2

--- src/main/java/com/example/springboot/controller/HelloController.java
+++ src/main/java/com/example/springboot/controller/HelloController.java
@@ -1,8 +1,14 @@
 package com.example.springboot.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @RestController
 public class HelloController {
@@ -10,4 +16,21 @@ public class HelloController {
 	public ResponseEntity<String> index() {
 		return ResponseEntity.ok("Good morning!");
 	}
+
+//	Exercise 1
+	@GetMapping(value = "/hello")
+	public String sayHello() {
+		return "Hello World!";
+	}
+
+	//Exercise 2
+	@GetMapping(value=  "/greeting" )
+	public ResponseEntity<String> greeting (){
+		if(new Date().getHours() > 11){
+			return ResponseEntity.status(HttpStatus.OK).body("Good Afternoon!");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body("Good Morning!");
+	}
+
+
 }
