4909b701fdd5dba612f5b31e464b2a1a5465c535
makenasandra <makenawachira@gmail.com>
2023-09-19 11:23:15 +0300

    exercise 4

---
+++ src/main/java/com/example/springboot/controller/Exercise1And2.java
@@ -0,0 +1,33 @@
+package com.example.springboot.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class Exercise1And2 {
+	@GetMapping(value = "/good-morning")
+	public ResponseEntity<String> index() {
+		return ResponseEntity.ok("Good morning!");
+	}
+
+//	Exercise 1
+	@GetMapping(value = "/hello")
+	public String sayHello() {
+		return "Hello World!";
+	}
+
+	//Exercise 2
+	@GetMapping(value=  "/greeting" )
+	public ResponseEntity<String> greeting (){
+		if(new Date().getHours() > 11){
+			return ResponseEntity.status(HttpStatus.OK).body("Good Afternoon!");
+		}
+		return ResponseEntity.status(HttpStatus.OK).body("Good Morning!");
+	}
+
+
+}
---
+++ src/main/java/com/example/springboot/controller/Exercise4.java
@@ -0,0 +1,20 @@
+package com.example.springboot.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+public class Exercise4 {
+    @GetMapping("/random")
+    public ResponseEntity<?> getStatus(){
+        if(new Random().nextBoolean()){
+            return ResponseEntity.status(200).build();
+        } else {
+            return ResponseEntity.status(400).build();
+        }
+
+    }
+}
